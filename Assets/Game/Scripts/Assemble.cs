2fb43024c2e30da2385da8c3db5ee894d829a84d
Pepetrom <140914691+Pepetrom@users.noreply.github.com>
2023-09-24 00:49:03 -0300

    go

--- Assets/Game/Scripts/Assemble.cs
+++ Assets/Game/Scripts/Assemble.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Assemble : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Storage storage;
+    [SerializeField] private int[] keyCodes;
+    [SerializeField] private bool[] keysUnlocked;
+    [SerializeField] private GameObject door;
+    private int openedKeysCount;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        storage = GetComponent<Storage>();
+        keysUnlocked = new bool[keyCodes.Length];
+        storage.Itens = new PickableIten[keyCodes.Length];
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                TestKey();
+            }
+        }
+    }
+    public void TestKey()
+    {
+        for (int i = 0; i < storage.Itens.Length; i++)
+        {
+            if (storage.Itens[i] != null)
+            {
+                for(int j = 0; j < keyCodes.Length; j++)
+                {
+                    Debug.Log("checou");
+                    if (keysUnlocked[j] != true && storage.Itens[i].id == keyCodes[j] )
+                    {
+                        Debug.Log("aberta");
+                        keysUnlocked[j] = true;
+                        //break;
+                    }
+                }
+            }
+            else
+            {
+                keysUnlocked[i] = false;
+            }
+        }
+        CheckUnlocked();
+    }
+    public void CheckUnlocked()
+    {
+        for (int i = 0; i < keyCodes.Length; i++)
+        {           
+            if (keysUnlocked[i])
+            {
+                openedKeysCount++;
+            }
+        }
+        if (openedKeysCount == keyCodes.Length)
+        {
+            OpenDoor();
+        }
+        openedKeysCount = 0;
+    }
+    public void OpenDoor()
+    {
+        door.SetActive(true);
+        Destroy(gameObject);
     }
 }
