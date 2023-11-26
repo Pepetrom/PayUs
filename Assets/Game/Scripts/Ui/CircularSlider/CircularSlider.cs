6adfe22488acbcfe14acdcc28d88edab0a277bf8
Pepetrom <140914691+Pepetrom@users.noreply.github.com>
2023-11-26 02:44:32 -0300

    A PORRA DO MENU TA AI

--- Assets/Game/Scripts/Ui/CircularSlider/CircularSlider.cs
+++ Assets/Game/Scripts/Ui/CircularSlider/CircularSlider.cs
@@ -7,18 +7,28 @@ public class CircularSlider : MonoBehaviour
     public Image fill;
     public float maxValue = 5;
     public float value = 5;
+    public Image hurt;
     private void Start()
     {
+        hurt.color = new Color(hurt.color.r, hurt.color.g, hurt.color.b, 0);
         fill.fillAmount = this.value / maxValue;
     }
     public void AddValue(float value)
     {
         this.value += value / maxValue;
-        fill.fillAmount = this.value/ maxValue;
+        fill.fillAmount = this.value / maxValue;
+        if (this.value < 1)
+        {
+            hurt.color = new Color(hurt.color.r, hurt.color.g, hurt.color.b, Mathf.Clamp((1 - (this.value * 0.33f)) / 1, 0, 0.1f));
+        }
     }
     public void RemoveValue(float value)
     {
         this.value += value / maxValue;
         fill.fillAmount = this.value / maxValue;
+        if (this.value < 1)
+        {
+            hurt.color = new Color(hurt.color.r, hurt.color.g, hurt.color.b, Mathf.Clamp((1 - (this.value * 0.33f)) / 1, 0, 0.1f));
+        }
     }
 }
